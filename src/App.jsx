4e03d5bfedad74ba6b9eae739a2bece6c12c2c8d
stsbk <stasboyko1@gmail.com>
2021-01-14 01:04:54 +0200

    Add all my files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
+import Home from "./Components/Pages/Home";
+import Services from "./Components/Pages/Services";
+import Products from "./Components/Pages/Products";
+import SignUp from "./Components/Pages/SignUp";
+
+function App() {
+  return (
+    <>
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/services' component={Services} />
+                <Route path='/products' component={Products} />
+                <Route path='/sign-up' component={SignUp} />
+                </Switch>
+        </Router>
+    </>
+  );
+}
+
+export default App;
